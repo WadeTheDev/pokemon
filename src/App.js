808b601fe695250c79685dc8bf5056127dca4c1a
WadeTheDev <echo1006j@gmail.com>
2022-08-02 14:44:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+// import { type } from '@testing-library/user-event/dist/type';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+const [pokemon, setpokemon] = useState(null)
+const [random, setRandom] = useState(1)
+useEffect(() => {
+  pokemonData()
+}, [])
+
+const RandomClickButton = () => {
+  const randomNumber = Math.floor(Math.random() * 151)
+  setRandom(randomNumber)
+  pokemonData()
+}
+
+  const pokemonData = async () => {
+    const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+    const response = await request.json()
+    setpokemon(response)
+  }
+  console.log(random)
+
+  if (pokemon === null) {
+    return( 
+      <div className='loader'>
+        <h1>LOADING</h1>
+      </div>
+    )
+  }else{
+    return (
+    <>
+    <div className='container'>
+    <div className='card'>
+      <img src={pokemon.sprites.other.dream_world.front_default}  alt={pokemon.name}/>
+      <div>
+      <h1>Name : {pokemon.species.name}</h1>
+      <p>Height : {pokemon.height}</p>
+      <p>Weight : {pokemon.weight}</p>
+      <ul>
+        {pokemon.types.map((typePokemon) => {
+          return(
+            <li>{typePokemon.type.name}</li>
+          )
+        })}
+      </ul>
+      </div>
+      <button onClick={RandomClickButton}> Pick Random Pokemon</button>
     </div>
-  );
+    </div>
+    </>
+  )}
 }
 
 export default App;
